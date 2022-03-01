6a0aad683248fd67847571240d39826c01ef5579
jangrzybek <jan@onspecta.com>
2022-03-01 06:02:35 +0100

    transition

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,5 @@
 # densenet_169
-wget -c https://www.dropbox.com/s/6gja02x160xvpg0/densenet_169_op11.tar.gz -O - | tar -xz
+wget -c https://ampereaimodelzoo.s3.amazonaws.com/densenet_169_op11.tar.gz -O - | tar -xz
 cp densenet_169_op11/densenet_169_op11.onnx classification/densenet_169
 rm -Rf densenet_169_op11
 
@@ -8,9 +8,9 @@ wget -c https://zenodo.org/record/2592612/files/resnet50_v1.onnx
 mv resnet50_v1.onnx classification/resnet_50_v1.5/resnet_50_v1.5_fp32.onnx
 
 # resnet_50_v1.5 fp16
-wget -c https://www.dropbox.com/s/r80ndhbht7tixn5/resnet_50_v1.5_fp16.onnx
+wget -c https://ampereaimodelzoo.s3.amazonaws.com/resnet_50_v1.5_fp16.onnx
 mv resnet_50_v1.5_fp16.onnx classification/resnet_50_v1.5
 
 # ssd_mobilenet_v1
-wget -c https://www.dropbox.com/s/om86cdc7qqlclh1/torch2onnx_ssd_mobilenet_v1.onnx
+wget -c https://ampereaimodelzoo.s3.amazonaws.com/torch2onnx_ssd_mobilenet_v1.onnx
 mv torch2onnx_ssd_mobilenet_v1.onnx object_detection/ssd_mobilenet_v1
